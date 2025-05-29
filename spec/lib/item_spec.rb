76cc79fc136d5e9dcb57ebefa38ae5c6a252a821
Mohnish G J <mail4mohnish@gmail.com>
2025-05-29 05:49:00 +0200

    Add an item with a name & price

--- spec/lib/item_spec.rb
+++ spec/lib/item_spec.rb
@@ -1,9 +1,10 @@
 describe Item do
   describe '#name' do
-    context 'give an item name' do
+    context 'give an item name & price' do
       it 'returns the item name' do
-        item = Item.new('Coke')
+        item = Item.new('Coke', 10.72)
         expect(item.name).to eq('Coke')
+        expect(item.price).to eq(10.72)
       end
     end
   end
