fd8ab934ef7c2bca56739291c7c181605a541f47
Mohnish G J <mail4mohnish@gmail.com>
2025-07-02 05:34:50 +0200

    Add ability to select an item from a vending machine
    - This covers a basic use case flow to select an item

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -6,5 +6,42 @@ class VendingMachine
     @balance = balance
   end
 
-  attr_reader :items, :balance
+  def select_item(item_name, amount)
+    item = items.find { |item| item.name == item_name }
+    if check_item_availability(item) && balance.amount >= item.price && item.quantity > 0
+      change = process_payment(item.price, amount)
+      [item.name, change.amount.cents, 'Thank you for your purchase!'] # TODO: adapt accordingly if change given how much amount and similarly other messages
+    else
+      'Item not available'
+    end
+  end
+
+  attr_reader :items
+  attr_accessor :balance
+
+  private
+
+  def check_item_availability(item)
+    if item && item.quantity > 0
+      true
+    else
+      false
+    end
+  end
+
+  def process_payment(item_price, amount) # TODO: Improve this logic to handle more use cases
+    change_to_give_user = calculate_change_to_give_user(amount, item_price)
+    update_machine_balance(amount, change_to_give_user.amount.cents)
+    change_to_give_user
+  end
+
+  def calculate_change_to_give_user(amount, item_price)
+    change_in_cents = amount - item_price.cents if amount > item_price.cents
+    change_in_cents > 0 ? Change.new(Money.new(change_in_cents, 'GBP')) : Change.new(Money.new(0, 'GBP'))
+  end
+
+  def update_machine_balance(amount, change_in_cents)
+    new_balance_cents = @balance.amount.cents + amount - (change_in_cents || 0)
+    @balance = Change.new(Money.new(new_balance_cents, 'GBP'))
+  end
 end
--- spec/lib/vending_machine_spec.rb
+++ spec/lib/vending_machine_spec.rb
@@ -1,13 +1,24 @@
 describe VendingMachine do
+  let(:items) { [Item.new('Coke', 150, 1), Item.new('Pepsi', 175, 1)] }
+  let(:balance) { Change.new(Money.new(1072, 'GBP')) }
+
   describe '#initialize' do
     context 'given a set of items & balance' do
-      let(:items) { [Item.new('Coke', 150), Item.new('Pepsi', 175)] }
-      let(:balance) { Change.new(Money.new(200, 'GBP')) }
-
       it 'returns the items & balance' do
         vending_machine = VendingMachine.new(items, balance)
         expect(vending_machine.items).to eq(items)
         expect(vending_machine.balance).to eq(balance)
+      end
+    end
+  end
+
+  describe 'selecting an item' do
+    context 'given an item name & balance' do
+      let(:selected_item) { Item.new('Coke', 150, 1) }
+
+      it 'returns the item & balance' do
+        vending_machine = VendingMachine.new(items, balance)
+        expect(vending_machine.select_item('Coke', 200)).to eq([selected_item.name, 50, 'Thank you for your purchase!'])
       end
     end
   end
