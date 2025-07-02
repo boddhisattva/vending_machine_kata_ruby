9bd4c7ade082413cf48467b71c8734d25234258b
Mohnish G J <mail4mohnish@gmail.com>
2025-07-02 05:33:53 +0200

    Add quantity field to Item

--- spec/lib/item_spec.rb
+++ spec/lib/item_spec.rb
@@ -2,11 +2,12 @@ describe Item do
   describe '#name and #price' do
     context 'given an item name & numeric price' do
       it 'returns the item name and price as Money object' do
-        item = Item.new('Coke', 1072)
+        item = Item.new('Coke', 1072, 5)
         expect(item.name).to eq('Coke')
         expect(item.price).to be_a(Money)
         expect(item.price.cents).to eq(1072)
         expect(item.price.currency.iso_code).to eq('GBP')
+        expect(item.quantity).to eq(5)
       end
     end
   end
