7c48134b66e7a6f22d62f927410ad41dab9fe6bb
Mohnish G J <mail4mohnish@gmail.com>
2025-07-02 05:33:53 +0200

    Add quantity field to Item

--- lib/item.rb
+++ lib/item.rb
@@ -1,10 +1,11 @@
 require 'money'
 
 class Item
-  def initialize(name, price)
+  def initialize(name, price, quantity)
     @name = name
     @price = price.is_a?(Money) ? price : Money.new(price, 'GBP')
+    @quantity = quantity
   end
 
-  attr_reader :name, :price
+  attr_reader :name, :price, :quantity
 end
