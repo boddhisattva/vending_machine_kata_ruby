db64e59e8759e3170ba9a2f2c19c28660a1f2b86
Mohnish G J <mail4mohnish@gmail.com>
2025-07-02 06:02:52 +0200

    Ensure item purchase works on giving exact amount

--- spec/lib/vending_machine_spec.rb
+++ spec/lib/vending_machine_spec.rb
@@ -16,9 +16,22 @@ describe VendingMachine do
     context 'given an item name & balance' do
       let(:selected_item) { Item.new('Coke', 150, 1) }
 
-      it 'returns the item & balance' do
-        vending_machine = VendingMachine.new(items, balance)
-        expect(vending_machine.select_item('Coke', 200)).to eq([selected_item.name, 50, 'Thank you for your purchase!'])
+      context 'when the item is available' do
+        context 'when the user pays more than the item price' do
+          it 'returns the item & balance' do
+            vending_machine = VendingMachine.new(items, balance)
+            expect(vending_machine.select_item('Coke',
+                                               200)).to eq([selected_item.name, 50, 'Thank you for your purchase!'])
+          end
+        end
+
+        context 'when the user pays the exact amount' do
+          it 'returns the item & balance' do
+            vending_machine = VendingMachine.new(items, balance)
+            expect(vending_machine.select_item('Coke',
+                                               150)).to eq([selected_item.name, 0, 'Thank you for your purchase!'])
+          end
+        end
       end
     end
   end
