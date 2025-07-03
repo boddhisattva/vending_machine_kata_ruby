debb72305a5320da1aa0e824ad0661b1e0aa9fb6
Mohnish G J <mail4mohnish@gmail.com>
2025-07-03 06:58:45 +0200

    Ensure item selection returns appropriate message when amount paid is less
    - Update message when amount given is equal or more & make other reltated refactoring changes

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -10,7 +10,7 @@ class VendingMachine
     item = items.find { |item| item.name == item_name }
     if check_item_availability(item) && balance.amount >= item.price && item.quantity > 0
       change = process_payment(item.price, amount)
-      [item.name, change.amount.cents, 'Thank you for your purchase!'] # TODO: adapt accordingly if change given how much amount and similarly other messages
+      change >= 0 ? confirm_payment(item, change) : specify_amount_pending(item, change)
     else
       'Item not available'
     end
@@ -21,6 +21,18 @@ class VendingMachine
 
   private
 
+  def confirm_payment(item, change)
+    if change > 0
+      "Thank you for your purchase of #{item.name}. Please collect your item and change: #{change}"
+    else
+      "Thank you for your purchase of #{item.name}. Please collect your item."
+    end
+  end
+
+  def specify_amount_pending(item, change)
+    "You need to pay #{change.abs} more cents to purchase #{item.name}"
+  end
+
   def check_item_availability(item)
     if item && item.quantity > 0
       true
@@ -30,14 +42,24 @@ class VendingMachine
   end
 
   def process_payment(item_price, amount) # TODO: Improve this logic to handle more use cases
-    change_to_give_user = calculate_change_to_give_user(amount, item_price)
-    update_machine_balance(amount, change_to_give_user.amount.cents)
-    change_to_give_user
+    change = if amount >= item_price.cents
+               calculate_change_to_give_user(amount,
+                                             item_price)
+             else
+               calculate_difference_amount_pending(
+                 amount, item_price
+               )
+             end
   end
 
   def calculate_change_to_give_user(amount, item_price)
     change_in_cents = amount > item_price.cents ? amount - item_price.cents : 0
-    change_in_cents > 0 ? Change.new(Money.new(change_in_cents, 'GBP')) : Change.new(Money.new(0, 'GBP'))
+    update_machine_balance(amount, change_in_cents)
+    change_in_cents
+  end
+
+  def calculate_difference_amount_pending(amount, item_price)
+    amount - item_price.cents
   end
 
   def update_machine_balance(amount, change_in_cents)
