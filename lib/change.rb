f98c289cea851b0e44225ef5fa84f17275b2a931
Mohnish G J <mail4mohnish@gmail.com>
2025-07-05 09:36:57 +0200

    Refactor code to use plain hash to store coins with quantity
    Add the Money class seems to overcomplicate things for a basic working version, remember: "Make it work, make it right, make it better", so just very basics right is core

--- lib/change.rb
+++ lib/change.rb
@@ -1,20 +1,25 @@
 require 'money'
 
+# 1072 --> 50 * 10 + 10 * 10 + 20  * 10 + 2 * 100 + 5* 10 + 2 * 10 + 1 * 2
+
 class Change
-  ACCEPTABLE_COINS = [
-    Money.new(1, 'GBP'),    # 1p
-    Money.new(2, 'GBP'),    # 2p
-    Money.new(5, 'GBP'),    # 5p
-    Money.new(10, 'GBP'),   # 10p
-    Money.new(20, 'GBP'),   # 20p
-    Money.new(50, 'GBP'),   # 50p
-    Money.new(100, 'GBP'),  # £1
-    Money.new(200, 'GBP')   # £2
-  ].freeze
+  DEFAULT_CURRENCY = 'GBP'
+
+  ACCEPTABLE_COINS = [50, 10, 20, 100, 200, 5, 2, 1].freeze
 
   def initialize(amount)
-    @amount = amount.is_a?(Money) ? amount : Money.new(amount, 'GBP')
+    unless coins_in_acceptable_denominations?(amount)
+      raise ArgumentError, "Please make sure coins are in acceptable denominations: #{ACCEPTABLE_COINS}"
+    end
+
+    @amount = amount
   end
 
   attr_reader :amount
+
+  private
+
+  def coins_in_acceptable_denominations?(amount)
+    amount.keys.all? { |coin_denomination| ACCEPTABLE_COINS.include?(coin_denomination) }
+  end
 end
--- spec/lib/change_spec.rb
+++ spec/lib/change_spec.rb
@@ -1,30 +1,62 @@
 require 'spec_helper'
 
 describe Change do
-  describe '#amount' do
-    context 'given a numeric amount' do
-      it 'returns the amount as Money object' do
-        change = Change.new(1072)
-        expect(change.amount).to be_a(Money)
-        expect(change.amount.cents).to eq(1072)
-        expect(change.amount.currency.iso_code).to eq('GBP')
+  let(:balance) { Change.new(balance_coins) }
+  let(:balance_coins) do
+    {
+      50 => 6,
+      10 => 10,
+      20 => 10,
+      100 => 2,
+      200 => 1,
+      5 => 10,
+      2 => 10,
+      1 => 2
+    }
+  end
+
+  describe '#initialize' do
+    context 'given a set of coins' do
+      it 'returns the total amount' do
+        expect(balance.amount).to eq(balance_coins)
+      end
+    end
+
+    context 'given invalid coin denominations' do
+      let(:invalid_coins) do
+        {
+          50 => 6,
+          10 => 10,
+          20 => 10,
+          100 => 2,
+          200 => 1,
+          5 => 10,
+          2 => 10,
+          1 => 2,
+          25 => 5 # Invalid denomination
+        }
+      end
+
+      it 'raises an ArgumentError with appropriate message' do
+        expect { Change.new(invalid_coins) }.to raise_error(
+          ArgumentError,
+          'Please make sure coins are in acceptable denominations: [50, 10, 20, 100, 200, 5, 2, 1]'
+        )
       end
     end
   end
-
-  describe 'ACCEPTABLE_COINS' do
-    it 'contains Money objects for valid UK denominations' do
-      expected_coins = [
-        Money.new(1, 'GBP'),    # 1p
-        Money.new(2, 'GBP'),    # 2p
-        Money.new(5, 'GBP'),    # 5p
-        Money.new(10, 'GBP'),   # 10p
-        Money.new(20, 'GBP'),   # 20p
-        Money.new(50, 'GBP'),   # 50p
-        Money.new(100, 'GBP'),  # £1
-        Money.new(200, 'GBP')   # £2
-      ]
-      expect(Change::ACCEPTABLE_COINS).to eq(expected_coins)
-    end
-  end
+  #   it 'contains Money objects for valid UK denominations' do
+  #     expected_coins = [
+  #       Money.new(1, 'GBP'),    # 1p
+  #       Money.new(2, 'GBP'),    # 2p
+  #       Money.new(5, 'GBP'),    # 5p
+  #       Money.new(10, 'GBP'),   # 10p
+  #       Money.new(20, 'GBP'),   # 20p
+  #       Money.new(50, 'GBP'),   # 50p
+  #       Money.new(100, 'GBP'),  # £1
+  #       Money.new(200, 'GBP')   # £2
+  #     ]
+  #     expect(Change::ACCEPTABLE_COINS).to eq(expected_coins)
+  #   end
+  # end
 end
