e70850b4dc38147148366fbdbfe4a19a69ba3eb6
Mohnish G J <mail4mohnish@gmail.com>
2025-08-07 08:29:03 +0200

    Remove unnecessary spec

--- spec/lib/item_spec.rb
+++ spec/lib/item_spec.rb
@@ -1,41 +1,13 @@
 require 'spec_helper'
-require_relative '../../lib/item'
 
 describe Item do
   describe '#name and #price' do
     context 'given an item name & numeric price' do
       it 'returns the item name and price as integer' do
-        item = Item.new('Coke', 150, 1)
+        item = Item.new('Coke', 150, 5)
         expect(item.name).to eq('Coke')
         expect(item.price).to eq(150)
-      end
-
-      it 'handles price in cents correctly' do
-        item = Item.new('Chips', 100, 1)  # 1 Euro = 100 cents
-        expect(item.price).to eq(100)
-      end
-
-      it 'handles float prices (though not recommended)' do
-        item = Item.new('Test', 1.5, 1)
-        expect(item.price).to eq(1.5)
-      end
-    end
-  end
-
-  describe '#quantity' do
-    context 'when modifying quantity' do
-      it 'allows quantity to be decremented' do
-        item = Item.new('Coke', 150, 2)
-        item.quantity -= 1
-        expect(item.quantity).to eq(1)
-      end
-
-      it 'prevents negative quantity' do
-        item = Item.new('Coke', 150, 1)
-        item.quantity -= 1
-        expect(item.quantity).to eq(0)
-        item.quantity -= 1
-        expect(item.quantity).to eq(-1)  # Note: This might need validation in the future
+        expect(item.quantity).to eq(5)
       end
     end
   end
