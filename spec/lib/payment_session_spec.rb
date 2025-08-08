e3cf5d56349220e7fb8bd5b164034347809ea263
Mohnish G J <mail4mohnish@gmail.com>
2025-08-08 20:51:49 +0200

    Refactor code to move some public methods to private interface
    Remove unnecessary specs as a result as some methods are unnecessarily public

--- spec/lib/payment_session_spec.rb
+++ spec/lib/payment_session_spec.rb
@@ -10,29 +10,12 @@ describe PaymentSession do
 
   it 'initializes with correct total needed' do
     expect(@session.total_needed).to eq(150)
-    expect(@session.calculate_remaining_amount).to eq(150)
-    expect(@session.total_paid).to eq(0)
   end
 
   it 'accumulates payments and calculates remaining' do
     @session.add_payment({ 100 => 1 })
-    expect(@session.calculate_remaining_amount).to eq(50)
     @session.add_payment({ 20 => 2 })
-    expect(@session.calculate_remaining_amount).to eq(10)
     @session.add_payment({ 10 => 1 })
-    expect(@session.calculate_remaining_amount).to eq(0)
     expect(@session.sufficient_funds?).to be true
-  end
-
-  it 'handles overpayment and calculates change' do
-    @session.add_payment({ 200 => 1 })
-    expect(@session.calculate_remaining_amount).to eq(0)
-    expect(@session.get_change_amount).to eq(50)
-  end
-
-  it 'handles exact payment' do
-    @session.add_payment({ 100 => 1, 50 => 1 })
-    expect(@session.calculate_remaining_amount).to eq(0)
-    expect(@session.get_change_amount).to eq(0)
   end
 end
